6d24a2bd0759bc1ba32c56e2231accac252f3835
Yahia-Derbala <yahderbala@gmail.com>
2023-12-16 17:35:16 +0200

    Hello.c Adding username

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, world - by Yahia-Derbala\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel world - by Yahia-Derbala\n");
 }
 
 module_init(hello_init);
